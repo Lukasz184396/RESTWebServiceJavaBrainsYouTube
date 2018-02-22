84c9e1cd901785be0f921813b27583d1c3a11794
ZawadaLukasz <lzzd@gft.com>
2018-02-22 11:53:08 +0100

    Lesson 22 PAgination and Filtering

--- src/main/java/org/koushik/javabrains/messenger/service/MessageService.java
+++ src/main/java/org/koushik/javabrains/messenger/service/MessageService.java
@@ -1,6 +1,7 @@
 package org.koushik.javabrains.messenger.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +19,26 @@ public class MessageService {
 	
 	public List<Message> getAllMessages() {
 		   return new ArrayList<Message>(   messages.values()  );
+	}
+	
+	public List<Message> getAllMessagesForYear(int year) {
+		List<Message> messsagesForYaer = new ArrayList<>();
+		Calendar calendar = Calendar.getInstance();
+		for(Message message : messages.values()) {
+			calendar.setTime(message.getCreated());
+			if(calendar.get(Calendar.YEAR) == year) {
+				messsagesForYaer.add(message);
+			}
+		}
+		
+		return messsagesForYaer;
+	}
+	
+	public List<Message> getAllMessagesPaginated(int start, int size) {
+		ArrayList<Message> list = new ArrayList<>(messages.values());
+		if(start + size > list.size()) return new ArrayList<Message>();
+		return list.subList(start,  start + size);
+		
 	}
 	
 	public Message addMessage(Message message) {
